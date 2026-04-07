a7176156fc4a808ee6ae87d38b1308717120a50f
Nazarii Dunas <dunas.nazar@gmail.com>
2026-04-07 11:51:10 +0200

    Added Dlegates, DelegateHandles, CallBacks and methods for Sesiion managing

--- Plugins/MySteamMultiplayerPlugin/Source/MySteamMultiplayerPlugin/Private/SteamSessionsSubsystem.cpp
+++ Plugins/MySteamMultiplayerPlugin/Source/MySteamMultiplayerPlugin/Private/SteamSessionsSubsystem.cpp
@@ -6,11 +6,58 @@
 #include "OnlineSubsystem.h"
 
 
-USteamSessionsSubsystem::USteamSessionsSubsystem()
+USteamSessionsSubsystem::USteamSessionsSubsystem():
+	//Binding callbacks to Delegates
+	CreateSessionCompleteDelegate(FOnCreateSessionCompleteDelegate::CreateUObject(this,&ThisClass::OnCreateSessionComplete)),
+	FindSessionCompleteDelegate(FOnFindSessionsCompleteDelegate::CreateUObject(this,&ThisClass::OnFindSessionComplete)),
+	JoinSessionCompleteDelegate(FOnJoinSessionCompleteDelegate::CreateUObject(this, &ThisClass::OnJoinSessionComplete)),
+	DestroySessionCompleteDelegate(FOnDestroySessionCompleteDelegate::CreateUObject(this, &ThisClass::OnDestroySessionComplete)),
+	StartSessionCompleteDelegate(FOnStartSessionCompleteDelegate::CreateUObject(this, &ThisClass::OnStartSessionComplete))
+
 {
 	if (IOnlineSubsystem* Subsystem = IOnlineSubsystem::Get())
 	{
 		SessionInterface = Subsystem->GetSessionInterface();
 	}
 	
+}
+
+void USteamSessionsSubsystem::CreateSession(int32 NumberOfPlayers, FString MatchType)
+{
+}
+
+void USteamSessionsSubsystem::FindSession(int32 MaxSearchResults)
+{
+}
+
+void USteamSessionsSubsystem::JoinSession(const FOnlineSessionSearchResult& SessionResult)
+{
+}
+
+void USteamSessionsSubsystem::DestroySession()
+{
+}
+
+void USteamSessionsSubsystem::StartSession()
+{
+}
+
+void USteamSessionsSubsystem::OnCreateSessionComplete(FName SessionName, bool Success)
+{
+}
+
+void USteamSessionsSubsystem::OnFindSessionComplete(bool Success)
+{
+}
+
+void USteamSessionsSubsystem::OnJoinSessionComplete(FName SessionName, EOnJoinSessionCompleteResult::Type Result)
+{
+}
+
+void USteamSessionsSubsystem::OnDestroySessionComplete(FName SessionName, bool Success)
+{
+}
+
+void USteamSessionsSubsystem::OnStartSessionComplete(FName SessionName, bool Success)
+{
 }
--- Plugins/MySteamMultiplayerPlugin/Source/MySteamMultiplayerPlugin/Public/SteamSessionsSubsystem.h
+++ Plugins/MySteamMultiplayerPlugin/Source/MySteamMultiplayerPlugin/Public/SteamSessionsSubsystem.h
@@ -18,9 +18,39 @@ class MYSTEAMMULTIPLAYERPLUGIN_API USteamSessionsSubsystem : public UGameInstanc
 	
 public:
 	USteamSessionsSubsystem();
-protected:
 	
+	void CreateSession(int32 NumberOfPlayers, FString MatchType);
+	void FindSession(int32 MaxSearchResults);
+	void JoinSession(const FOnlineSessionSearchResult& SessionResult);
+	void DestroySession();
+	void StartSession();
+	
+protected:
+	//Callbacks for Delegates 
+	void OnCreateSessionComplete(FName SessionName, bool Success);
+	void OnFindSessionComplete(bool Success);
+	void OnJoinSessionComplete(FName SessionName, EOnJoinSessionCompleteResult::Type Result);
+	void OnDestroySessionComplete(FName SessionName, bool Success);
+	void OnStartSessionComplete(FName SessionName, bool Success);
 private:
 	IOnlineSessionPtr SessionInterface;
+	
+	//Delegates for OnlineSessionInterface
+	
+	//Create
+	FOnCreateSessionCompleteDelegate CreateSessionCompleteDelegate;
+	FDelegateHandle CreateSessionCompleteDelegateHandle;
+	//Find
+	FOnFindSessionsCompleteDelegate FindSessionCompleteDelegate;
+	FDelegateHandle FindSessionCompleteDelegateHandle;
+	//Join
+	FOnJoinSessionCompleteDelegate JoinSessionCompleteDelegate;
+	FDelegateHandle JoinSessionCompleteDelegateHandle;
+	//Destroy
+	FOnDestroySessionCompleteDelegate DestroySessionCompleteDelegate;
+	FDelegateHandle DestroySessionCompleteDelegateHandle;
+	//Start
+	FOnStartSessionCompleteDelegate StartSessionCompleteDelegate;
+	FDelegateHandle StartSessionCompleteDelegateHandle;
 	
 };
